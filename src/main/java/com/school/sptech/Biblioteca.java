caae2593d1716292170368342aaa74d5fcca683c
CaioSousavs <joao.sousa@sptech.school>
2025-08-31 11:28:32 -0300

    Exercicio concluido

---
+++ src/main/java/com/school/sptech/Biblioteca.java
@@ -0,0 +1,79 @@
+package com.school.sptech;
+
+public class Biblioteca {
+    private String nome;
+    private Double multaDiaria;
+    private Integer qtdLivros;
+    private Boolean ativa;
+
+    public String getNome(){
+        return nome;
+    }
+    public Double getMultaDiaria(){
+        return multaDiaria;
+    }
+    public Integer getQtdLivros(){
+        return qtdLivros;
+    }
+    public Boolean getAtiva(){
+        return ativa;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+
+
+    public Biblioteca(String nome, Double multaDiaria){
+        this.nome = nome;
+        this.multaDiaria = multaDiaria;
+        qtdLivros = 0;
+        ativa = true;
+    }
+
+    public void registrarLivro(Integer quantidade){
+
+        if(quantidade != null && quantidade > 0 && ativa){
+            qtdLivros += quantidade;
+        }
+    }
+
+    public Integer emprestar(Integer quantidade){
+        if(ativa && quantidade != null && quantidade > 0 && quantidade <= qtdLivros ){
+            qtdLivros -= quantidade;
+            return quantidade;
+        }else {
+            return null;
+        }
+    }
+    public Integer devolver(Integer quantidade){
+        if(ativa && quantidade != null && quantidade > 0 ){
+            qtdLivros += quantidade;
+        }
+        return quantidade;
+    }
+    public Integer desativar(){
+        if(ativa == false){
+            return null;
+        }else{
+            ativa = false;
+            Integer qtdAnterior = qtdLivros;
+            qtdLivros *= 0;
+            return qtdAnterior;
+        }
+    }
+    public void transferir(Biblioteca destino, Integer quantidade){
+        if(ativa && destino.ativa && quantidade != null && quantidade > 0 && quantidade <= qtdLivros){
+            qtdLivros -= quantidade;
+            destino.qtdLivros += quantidade;
+        }
+    }
+    public Boolean reajustarMulta(Double percentual){
+        if(percentual != null && percentual > 0){
+            multaDiaria *= 1.10;
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
